895d081227183394922bb34ceacd6a04607573e6
Loic Pottier <pottier1@llnl.gov>
2023-08-15 19:32:30 -0700

    Improved script. Tested with Flux 0.52.0
    Signed-off-by: Loic Pottier <pottier1@llnl.gov>

--- scripts/bootstrap_flux.sh
+++ scripts/bootstrap_flux.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-usage="Usage: $(basename "$0") [NNODES] [JSON file] -- Script that bootstrap Flux on NNODES and writes Flux URIs to the JSON file."
+usage="Usage: $(basename "$0") [#NODES] [JSON file] -- Script that bootstrap Flux on NNODES and writes Flux URIs to the JSON file."
+
+function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
 
 # the script needs the number of nodes for flux
 FLUX_NODES="$1"
@@ -8,6 +10,8 @@ FLUX_NODES="$1"
 AMS_JSON="$2"
 FLUX_SERVER="ams-uri.log"
 FLUX_LOG="ams-flux.log"
+# Flux-core Minimum version required by AMS
+MIN_VAR_FLUX="0.48.0"
 
 re='^[0-9]+$'
 if ! [[ $FLUX_NODES =~ $re ]] ; then
@@ -31,8 +35,17 @@ if ! [[ -x "$(command -v flux)" ]]; then
   echo "[$(date +'%m%d%Y-%T')@$(hostname)] Error: flux is not installed."
   exit 1
 fi
-
 echo "[$(date +'%m%d%Y-%T')@$(hostname)] flux = $(which flux)"
+
+flux_version=$(version $(flux version | awk '/^commands/ {print $2}'))
+MIN_VAR_FLUX=$(version ${MIN_VAR_FLUX})
+# We need to remove leading 0 because they are interpreted as octal numbers in bash
+if [[ "${flux_version#00}" -lt "${MIN_VAR_FLUX#00}" ]]; then
+  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Error Flux $flux_version is not supported.\
+  AMS requires at least Flux $MIN_VAR_FLUX".
+  exit 1
+fi
+
 echo "[$(date +'%m%d%Y-%T')@$(hostname)] flux version"
 flux version
 
@@ -59,7 +72,6 @@ if [[ "$MACHINE" == "lassen" ]] ; then
     jsrun -a 1 -c ALL_CPUS -g ALL_GPUS -n ${FLUX_NODES} \
       --bind=none --smpiargs="-disable_gpu_hooks" \
       flux start -o,-S,log-filename=$FLUX_LOG -v $FLUX_SLEEP_WRAPPER $FLUX_SERVER &
-    
 elif [[ "$MACHINE" == "pascal" || "$MACHINE" == "ruby" ]] ; then
     srun -n ${FLUX_NODES} -N ${FLUX_NODES} --pty --mpi=none --mpibind=off \
       flux start -o,-S,log-filename=$FLUX_LOG -v $FLUX_SLEEP_WRAPPER $FLUX_SERVER &
@@ -96,7 +108,7 @@ NODES_ML=$(jq ".ml.nodes" $AMS_JSON)
 NODES_CONTAINERS=$(jq ".containers.nodes" $AMS_JSON)
 re='^[0-9]+$'
 if ! [[ $NODES_PHYSICS =~ $re && $NODES_ML =~ $re && $NODES_CONTAINERS =~ $re ]] ; then
-  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Error: number of nodes is not an integer \
+  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Error: number of nodes is not an integer\
   (${NODES_PHYSICS}, ${NODES_ML}, ${NODES_CONTAINERS})"
   exit 1
 fi
@@ -105,7 +117,7 @@ CORES_PHYSICS=$(jq ".physics.cores" $AMS_JSON)
 CORES_ML=$(jq ".ml.cores" $AMS_JSON)
 CORES_CONTAINERS=$(jq ".containers.cores" $AMS_JSON)
 if ! [[ $CORES_PHYSICS =~ $re && $CORES_ML =~ $re && $CORES_CONTAINERS =~ $re ]] ; then
-  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Error: number of cores is not an integer \
+  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Error: number of cores is not an integer\
   (${CORES_PHYSICS}, ${CORES_ML}, ${CORES_CONTAINERS})"
   exit 1
 fi
@@ -114,7 +126,7 @@ GPUS_PHYSICS=$(jq ".physics.gpus" $AMS_JSON)
 GPUS_ML=$(jq ".ml.gpus" $AMS_JSON)
 GPUS_CONTAINERS=$(jq ".containers.gpus" $AMS_JSON)
 if ! [[ $GPUS_PHYSICS =~ $re && $GPUS_ML =~ $re && $CORES_CONTAINERS =~ $re ]] ; then
-  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Error: number of GPUs is not an integer \
+  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Error: number of GPUs is not an integer\
   (${GPUS_PHYSICS}, ${GPUS_ML}, ${GPUS_CONTAINERS})"
   exit 1
 fi
@@ -124,54 +136,57 @@ fi
 # but flux alloc --bg is only avalable with the recent flux releases
 # TODO: move  to flux alloc
 JOBID_PHYSICS=$(
-    flux mini batch --job-name="ams-physics" \
+    flux batch --job-name="ams-physics" \
     --output="ams-physics-{{id}}.log" \
     --nslots=1 --nodes=$NODES_PHYSICS \
     --cores-per-slot=$CORES_PHYSICS \
     --gpus-per-slot=$GPUS_PHYSICS \
     --wrap sleep inf
 )
+sleep 3s
+
 if [[ "$(flux jobs --no-header -o '{status}' $JOBID_PHYSICS)" == "RUN" ]]; then
   FLUX_PHYSICS_URI=$(flux uri --remote $JOBID_PHYSICS)
-  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Physics batch job ($JOBID_PHYSICS) \
+  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Physics batch job ($JOBID_PHYSICS)\
   started with nodes=${NODES_PHYSICS}, cores=${CORES_PHYSICS}, gpus=${GPUS_PHYSICS}"
 else
-  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Failed to launch physics batch job ($JOBID_PHYSICS)"
-  exit 1
+  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Warning: Failed to launch physics batch job ($JOBID_PHYSICS)"
 fi
 
 JOBID_ML=$(
-    flux mini batch --job-name="ams-ml" \
+    flux batch --job-name="ams-ml" \
     --output="ams-ml-{{id}}.log" \
     --nslots=1 --nodes=$NODES_ML\
     --cores-per-slot=$CORES_ML \
     --gpus-per-slot=$GPUS_ML \
     --wrap sleep inf
 )
+sleep 3s
+
 if [[ "$(flux jobs --no-header -o '{status}' $JOBID_ML)" == "RUN" ]]; then
   FLUX_ML_URI=$(flux uri --remote $JOBID_ML)
-  echo "[$(date +'%m%d%Y-%T')@$(hostname)] ML batch job ($JOBID_ML) \
+  echo "[$(date +'%m%d%Y-%T')@$(hostname)] ML batch job ($JOBID_ML)\
   started with nodes=${NODES_ML}, cores=${CORES_ML}, gpus=${GPUS_ML}"
 else
-  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Failed to launch ML batch job ($JOBID_ML)"
-  exit 1
+  echo "[$(date +'%m%d%Y-%T')@$(hostname)]  Warning: Failed to launch ML batch job ($JOBID_ML)"
 fi
 
 JOBID_CONTAINERS=$(
-    flux mini batch --job-name="ams-containers" \
+    flux batch --job-name="ams-containers" \
     --output="ams-containers-{{id}}.log" \
     --nslots=1 --nodes=$NODES_CONTAINERS \
     --cores-per-slot=$CORES_CONTAINERS \
     --gpus-per-slot=$GPUS_CONTAINERS \
     --wrap sleep inf
 )
+sleep 3s
+
 if [[ "$(flux jobs --no-header -o '{status}' $JOBID_CONTAINERS)" == "RUN" ]]; then
   FLUX_CONTAINERS_URI=$(flux uri --remote $JOBID_CONTAINERS)
-  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Containers batch job ($JOBID_CONTAINERS) \
+  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Containers batch job ($JOBID_CONTAINERS)\
   started with nodes=${NODES_CONTAINERS}, cores=${CORES_CONTAINERS}, gpus=${GPUS_CONTAINERS}"
 else
-  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Failed to launch containers batch job ($JOBID_CONTAINERS)"
-  exit 1
+  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Warning: Failed to launch containers batch job ($JOBID_CONTAINERS)"
 fi
 
 # Add all URIs to existing AMS JSON file
@@ -181,10 +196,10 @@ jq '. += {flux:{}}' $AMS_JSON > $AMS_JSON_BCK && cp $AMS_JSON_BCK $AMS_JSON
 jq --arg flux_uri "$FLUX_URI" '.flux += {"global_uri":$flux_uri}' $AMS_JSON > $AMS_JSON_BCK && cp $AMS_JSON_BCK $AMS_JSON
 jq --arg flux_uri "$FLUX_PHYSICS_URI" '.flux += {"physics_uri":$flux_uri}' $AMS_JSON > $AMS_JSON_BCK && cp $AMS_JSON_BCK $AMS_JSON
 jq --arg flux_uri "$FLUX_ML_URI" '.flux += {"ml_uri":$flux_uri}' $AMS_JSON > $AMS_JSON_BCK && cp $AMS_JSON_BCK $AMS_JSON
-jq --arg flux_uri "$FLUX_CONTAINERS_URI" '.flux += {"container_uri":$flux_uri}' $AMS_JSON > $AMS_JSON && cp $AMS_JSON_BCK $AMS_JSON
+jq --arg flux_uri "$FLUX_CONTAINERS_URI" '.flux += {"container_uri":$flux_uri}' $AMS_JSON > $AMS_JSON_BCK && cp $AMS_JSON_BCK $AMS_JSON
 
 # We move the file only if jq is sucessful otherwise jq will likey erase the original file
-if [[ $? -ne 0 ]]; then
+if [[ "$?" -ne 0 ]]; then
   mv -f $AMS_JSON_BCK $AMS_JSON && rm -f $AMS_JSON_BCK
 fi
 
