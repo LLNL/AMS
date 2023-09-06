44c15e648f57720e5e41163b6e6efdd2d824091a
Loic Pottier <pottier1@llnl.gov>
2023-09-05 21:36:55 -0700

    Fixed several problems in AMS packages and added a AMSBroker to send messages to a RabbitMQ queue
    Signed-off-by: Loic Pottier <pottier1@llnl.gov>

--- src/AMSWorkflow/ams/rmq.py
+++ src/AMSWorkflow/ams/rmq.py
@@ -10,104 +10,102 @@ import os
 import logging
 import json
 
+class RMQChannel:
+    """
+        A wrapper around RMQ channel
+    """
+
+    def __init__(self, connection, q_name):
+        self.connection = connection
+        self.q_name = q_name
+
+    def __enter__(self):
+        self.open()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    @staticmethod
+    def callback(method, properties, body):
+        return body.decode("utf-8")
+
+    def open(self):
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue = self.q_name)
+
+    def close(self):
+        self.channel.close()
+
+    def receive(self, n_msg: int = None, accum_msg = list()):
+        """
+        Consume a message on the queue and post processing by calling the callback.
+        @param n_msg The number of messages to receive.
+            - if n_msg is None, this call will block for ever and will process all messages that arrives
+            - if n_msg = 1 for example, this function will block until one message has been processed.
+        @return a list containing all received messages
+        """
+
+        if self.channel and self.channel.is_open:
+            self.logger.info(
+                f"Starting to consume messages from queue={self.q_name}, routing_key={self.routing_key} ..."
+            )
+            # we will consume only n_msg and requeue all other messages
+            # if there are more messages in the queue.
+            # It will block as long as n_msg did not get read
+            if n_msg:
+                n_msg = max(n_msg, 0)
+                message_consumed = 0
+                # Comsume n_msg messages and break out
+                for method_frame, properties, body in self.channel.consume(self.q_name):
+                    # Call the call on the message parts
+                    try:
+                        accum_msg.append(
+                            RMQClient.callback(
+                                method_frame,
+                                properties,
+                                body,
+                            )
+                        )
+                    except Exception as e:
+                        self.logger.error(f"Exception {type(e)}: {e}")
+                        self.logger.debug(traceback.format_exc())
+                    finally:
+                        # Acknowledge the message even on failure
+                        self.channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+                    self.logger.warning(
+                        f"Consumed message {message_consumed+1}/{method_frame.delivery_tag} (exchange={method_frame.exchange}, routing_key={method_frame.routing_key})"
+                    )
+                    message_consumed += 1
+                    # Escape out of the loop after nb_msg messages
+                    if message_consumed == n_msg:
+                        # Cancel the consumer and return any pending messages
+                        self.channel.cancel()
+                        break
+        return accum_msg
+
+    def send(self, text: str):
+        """
+        Send a message
+        @param text The text to send
+        """
+        self.channel.basic_publish(exchange="", routing_key=self.q_name, body=text)
+        return
+
+    def get_messages(self):
+        return  # messages
+
+    def purge(self):
+        """Removes all the messages from the queue."""
+        if self.channel and self.channel.is_open:
+            self.channel.queue_purge(self.q_name)
+
 
 class RMQClient:
     """
     RMQClient is a class that manages the RMQ client lifecycle.
     """
-
-    class RMQChannel:
-        """
-            A wrapper around RMQ channel
-        """
-
-        def __init__(self, connection, q_name):
-            self.q_name = q_name
-
-        def __enter__(self):
-            self.open()
-            return self
-
-        def __exit__(self):
-            self.close()
-
-        @staticmethod
-        def callback(method, properties, body):
-            return body.decode("utf-8")
-
-        def open(self):
-            self.channel = connection.channel()
-            self.channel.queue_declare(queue=q_name)
-
-        def close(self):
-            self.channel.close()
-
-        def receive(self, n_msg: int = None, accum_msg = list()):
-            """
-            Consume a message on the queue and post processing by calling the callback.
-            @param n_msg The number of messages to receive.
-                - if n_msg is None, this call will block for ever and will process all messages that arrives
-                - if n_msg = 1 for example, this function will block until one message has been processed.
-            @return a list containing all received messages
-            """
-
-            if self.channel and self.channel.is_open:
-                self.logger.info(
-                    f"Starting to consume messages from queue={self.q_name}, routing_key={self.routing_key} ..."
-                )
-                # we will consume only n_msg and requeue all other messages
-                # if there are more messages in the queue.
-                # It will block as long as n_msg did not get read
-                if n_msg:
-                    n_msg = max(n_msg, 0)
-                    message_consumed = 0
-                    # Comsume n_msg messages and break out
-                    for method_frame, properties, body in self.channel.consume(self.q_name):
-                        # Call the call on the message parts
-                        try:
-                            accum_msg.append(
-                                RMQClient.RMQChannel(
-                                    method_frame,
-                                    properties,
-                                    body,
-                                )
-                            )
-                        except Exception as e:
-                            self.logger.error(f"Exception {type(e)}: {e}")
-                            self.logger.debug(traceback.format_exc())
-                        finally:
-                            # Acknowledge the message even on failure
-                            self.channel.basic_ack(delivery_tag=method_frame.delivery_tag)
-                        self.logger.warning(
-                            f"Consumed message {message_consumed+1}/{method_frame.delivery_tag} (exchange={method_frame.exchange}, routing_key={method_frame.routing_key})"
-                        )
-                        message_consumed += 1
-                        # Escape out of the loop after nb_msg messages
-                        if message_consumed == n_msg:
-                            # Cancel the consumer and return any pending messages
-                            self.channel.cancel()
-                            break
-            return accum_msg
-
-        def send(self, text):
-            """
-            Send \p text
-            """
-
-            self.channel.basic_publish(exchange="", routing_key=self.q_name, body=text)
-
-            return
-
-        def get_messages(self):
-            return  # messages
-
-        def purge(self):
-            """Removes all the messages from the queue."""
-
-            if self.channel and self.channel.is_open:
-                self.channel.queue_purge(self.q_name)
-
-    def __init__(self, vhost, port, user, password, cert, logger: logging.Logger = None):
+    def __init__(self, host, port, vhost, user, password, cert, logger: logging.Logger = None):
         # CA Cert, can be generated with (where $REMOTE_HOST and $REMOTE_PORT can be found in the JSON file):
         # openssl s_client -connect $REMOTE_HOST:$REMOTE_PORT -showcerts < /dev/null 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' rmq-pds.crt
         self.logger = logger if logger else logging.getLogger(__name__)
@@ -115,6 +113,7 @@ class RMQClient:
         self.context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
         self.context.verify_mode = ssl.CERT_REQUIRED
         self.context.load_verify_locations(self.cert)
+        self.host = host
         self.vhost = vhost
         self.port = port
         self.user = user
@@ -135,7 +134,7 @@ class RMQClient:
         self.connection = pika.BlockingConnection(self.connection_params)
         return self
 
-    def __exit__(self):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         self.connection.close()
 
     def connect(self, queue):
---
+++ src/AMSWorkflow/ams_wf/AMSBroker.py
@@ -0,0 +1,82 @@
+# Copyright 2021-2023 Lawrence Livermore National Security, LLC and other
+# AMSLib Project Developers
+#
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import argparse
+import json
+import os
+import sys
+
+from ams.rmq import RMQClient
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="AMS Broker interface to send/receive messages."
+    )
+
+    parser.add_argument(
+        "-c",
+        "--config",
+        dest="config",
+        help="Path to broker configuration file",
+        required=True,
+    )
+
+    parser.add_argument(
+        "-t",
+        "--certificate",
+        dest="certificate",
+        help="Path to TLS certificate file",
+        required=True,
+    )
+
+    parser.add_argument(
+        "-s",
+        "--send",
+        dest="msg_send",
+        type=str,
+        help="Message to send",
+        required=True,
+    )
+
+    parser.add_argument(
+        "-q",
+        "--queue",
+        dest="queue",
+        type=str,
+        help="Queue to which the message will be sent",
+        required=True,
+    )
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.config):
+        print(f"Error: config file {args.config} does not exist")
+        sys.exit(1)
+
+    if not os.path.isfile(args.certificate):
+        print(f"Error: certificate file {args.certificate} does not exist")
+        sys.exit(1)
+
+    with open(args.config, "r") as fd:
+        config = json.load(fd)
+    
+    host = config["service-host"]
+    vhost = config["rabbitmq-vhost"]
+    port = config["service-port"]
+    user = config["rabbitmq-user"]
+    password = config["rabbitmq-password"]
+
+    with RMQClient(host, port, vhost, user, password, args.certificate) as client:
+        with client.connect(args.queue) as channel:
+            channel.send(args.msg_send)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupted")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
