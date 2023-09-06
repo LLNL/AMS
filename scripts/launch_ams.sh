d697fdb591f317962e9b9f8da044891dcd71fa36
Loic Pottier <pottier1@llnl.gov>
2023-09-05 22:54:10 -0700

    Upgraded general script to communicate with RabbitMQ
    Signed-off-by: Loic Pottier <pottier1@llnl.gov>

--- scripts/launch_ams.sh
+++ scripts/launch_ams.sh
@@ -11,6 +11,7 @@ FLUX_NODES="$1"
 # JSON configuration for AMS that will get updated by this script
 AMS_JSON="$2"
 UPDATE_SECRETS=rmq_add_secrets.sh
+# Ssh bridge could be needed if OpenShift is not reachable from every clusters.
 SSH_BRIDGE="ssh quartz"
 BOOTSTRAP=bootstrap_flux.sh
 START_PHYSICS=launch_physics.sh
@@ -35,15 +36,49 @@ export FLUX_SSH="ssh"
 USE_DB=$(jq -r ".ams_app.use_db" $AMS_JSON)
 DBTYPE=$(jq -r ".ams_app.dbtype" $AMS_JSON)
 
+# 1. If we use RabbitMQ and asynchronous traning we add needed secrets OpenShift so AMS daemon can connect to RabbitMQ
 if [[ $USE_DB && $DBTYPE = "rmq" ]]; then
   echo "[$(date +'%m%d%Y-%T')@$(hostname)] Trying to update secrets on OpenShift"
   $SSH_BRIDGE $UPDATE_SECRETS $AMS_JSON
 fi
 
+# 2. We bootstrap Flux on FLUX_NODES nodes
 $BOOTSTRAP $FLUX_NODES $AMS_JSON
 
-#TODO: send data to RabbitMQ here
+# 3. We send the current UID and the Flux ML URI to the AMS daemon listening
+RMQ_TMP="rmq.json"
+CERT_TLS="rmq.cert"
+# TODO: create a specific queue for the AMS daemon to listen to
+AMS_DAEMON_QUEUE="test3"
+# This require to install the AMS python package
+AMS_BROKER_EXE="AMSBroker"
 
+if [[ $USE_DB && $DBTYPE = "rmq" ]]; then
+  RMQ_CONFIG=$(jq ".rabbitmq" $AMS_JSON)
+  echo $RMQ_CONFIG > $RMQ_TMP
+  echo "[$(date +'%m%d%Y-%T')@$(hostname)] Extracted RabbitMQ configuration to ${RMQ_TMP}"
+
+  REMOTE_HOST=$(jq ".rabbitmq.service-host" $AMS_JSON)
+  REMOTE_PORT=$(jq ".rabbitmq.service-port" $AMS_JSON)
+  openssl s_client -connect ${REMOTE_HOST}:${REMOTE_PORT} -showcerts < /dev/null 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' ${CERT_TLS}
+  if [[ "$?" -ne 0 ]]; then
+    echo "[$(date +'%m%d%Y-%T')@$(hostname)] Successfuly generated TLS certificate written in ${CERT_TLS}"
+  else
+    echo "[$(date +'%m%d%Y-%T')@$(hostname)] Error during TLS certificate generation"
+    exit 1
+  fi
+  AMS_UID=$(id -u)
+  AMS_ML_URI=$(jq -r ".flux.ml_uri" $AMS_JSON)
+  MSG="{\"uid\": ${AMS_UID}, \"ml_uri\": ${AMS_ML_URI}}"
+  ${AMS_BROKER_EXE} -c ${RMQ_TMP} -t ${CERT_TLS} -q ${AMS_DAEMON_QUEUE} -s ${MSG}
+  if [[ "$?" -ne 0 ]]; then
+    echo "[$(date +'%m%d%Y-%T')@$(hostname)] Successfuly sent message ${MSG} to queue ${AMS_DAEMON_QUEUE}"
+  else
+    echo "[$(date +'%m%d%Y-%T')@$(hostname)] Error: message did not get send to RabbitMQ"
+    exit 1
+  fi
+fi
+
+# 4. We start the physics code 
 $START_PHYSICS $AMS_JSON
-
-echo "[$(date +'%m%d%Y-%T')@$(hostname)] AMS workflow is ready!"
+echo "[$(date +'%m%d%Y-%T')@$(hostname)] AMS workflow is ready to run!"
