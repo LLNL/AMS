801da948aa5c412448cbc13699f70454edd1c9fe
Loic Pottier <pottier1@llnl.gov>
2023-09-05 21:36:55 -0700

    Fixed several problems in AMS packages and added a AMSBroker to send messages to a RabbitMQ queue
    Signed-off-by: Loic Pottier <pottier1@llnl.gov>

--- src/AMSWorkflow/setup.py
+++ src/AMSWorkflow/setup.py
@@ -11,10 +11,11 @@ setuptools.setup(
     packages=['ams_wf', 'ams'],
     install_requires = [
         'argparse',
-        'pika',
+        'pika>=1.3.0',
         ],
     entry_points={
         'console_scripts': [
+            'AMSBroker=ams_wf.AMSBroker:main',
             'AMSDBStage=ams_wf.AMSDBStage:main',
             'AMSOrchestrator=ams_wf.AMSOrchestrator:main',
             'AMSTrain=ams_wf.AMSTrain:main']
