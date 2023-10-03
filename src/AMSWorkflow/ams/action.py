6a053d5d74f8a396f3f50fe53ca5fabce941d88b
Konstantinos Parasyris <parasyris1@llnl.gov>
2023-10-03 13:56:02 -0700

    Action abstraction class

---
+++ src/AMSWorkflow/ams/action.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# Copyright 2021-2023 Lawrence Livermore National Security, LLC and other
+# AMSLib Project Developers
+#
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+from abc import ABC, abstractmethod
+
+
+class UserAction(ABC):
+    """
+    An abstract class used as an interface from users
+    to describe actions to be peformed in the data-pipeline
+    of the AMS staging mechanism
+    """
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def __call__(self, inputs, outputs):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def add_cli_args(parser):
+        """ """
+        pass
+
+    @classmethod
+    @abstractmethod
+    def from_cli(cls, args):
+        pass
