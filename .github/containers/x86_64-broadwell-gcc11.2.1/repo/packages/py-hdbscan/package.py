eabfd4578389d2362cbfefa34e5a59844ca4b4bd
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2023-10-11 19:52:59 -0700

    Fix for comments

--- .github/containers/x86_64-broadwell-gcc11.2.1/repo/packages/py-hdbscan/package.py
+++ .github/containers/x86_64-broadwell-gcc11.2.1/repo/packages/py-hdbscan/package.py
@@ -1,8 +1,7 @@
 # Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# AMSLib Project Developers. See the top-level COPYRIGHT file for details.
 #
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 from spack.package import *
 
