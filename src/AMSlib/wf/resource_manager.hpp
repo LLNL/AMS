5beca79942ac560ee4e1b01ae610940d0ff29e7d
Aliza Lisan <lisan1@lassen708.coral.llnl.gov>
2024-01-29 22:41:15 -0800

    remove param 'dev'

--- src/AMSlib/wf/resource_manager.hpp
+++ src/AMSlib/wf/resource_manager.hpp
@@ -145,7 +145,7 @@ public:
   static void deallocate(std::vector<T*>& dPtr, AMSResourceType resource)
   {
     for (auto* I : dPtr)
-      RMAllocators[resource]->deallocate(I, dev);
+      RMAllocators[resource]->deallocate(I);
   }
 
   static void init()
