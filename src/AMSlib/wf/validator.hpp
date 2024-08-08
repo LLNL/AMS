5ae6a2c859b8016c2f89018a14112ba6229aacc0
Jae-Seung Yeom <yeom2@llnl.gov>
2024-08-08 10:22:34 -0700

    Compute error statistics on the points originally evaluated by the physics model

--- src/AMSlib/wf/validator.hpp
+++ src/AMSlib/wf/validator.hpp
@@ -2,9 +2,12 @@
 #define __AMS_VALIDATOR_HPP__
 #include <iostream>
 #include <string>
+#include <cstring> // memcpy
 #include <vector>
+#include <array>
 #include <algorithm>
 #include <mpi.h>
+#include <type_traits> // is_same
 
 #if __STANDALONE_TEST__
 namespace ams
@@ -53,18 +56,20 @@ template <typename FPTypeValue>
 struct ValStats
 {
   ValStats()
-  : m_cnt(0u),
-    m_avg(static_cast<FPTypeValue>(0)),
-    m_var(static_cast<FPTypeValue>(0))
+  : m_cnt({0u,0u}),
+    m_avg({static_cast<FPTypeValue>(0), static_cast<FPTypeValue>(0)}),
+    m_var({static_cast<FPTypeValue>(0), static_cast<FPTypeValue>(0)})
   {}
 
-  ValStats(unsigned int cnt, FPTypeValue avg, FPTypeValue var)
+  ValStats(const std::array<unsigned int, 2>& cnt,
+           const std::array<FPTypeValue, 2>& avg,
+           const std::array<FPTypeValue, 2>& var)
   : m_cnt(cnt), m_avg(avg), m_var(var)
   {}
 
-  unsigned int m_cnt;
-  FPTypeValue m_avg;
-  FPTypeValue m_var;
+  std::array<unsigned int, 2> m_cnt;
+  std::array<FPTypeValue, 2> m_avg;
+  std::array<FPTypeValue, 2> m_var;
 };
 
 template <typename FPTypeValue>
@@ -122,12 +127,20 @@ public:
                       unsigned k_v);
 #if __STANDALONE_TEST__
   void backup_surrogate_outs(const FPTypeValue* sur_out,
-                             StepValPoints<FPTypeValue>& step_valpoints) const;
+                             StepValPoints<FPTypeValue>& step_valpoints);
+  std::array<FPTypeValue, 2> backup_validation_outs(const FPTypeValue* phy_out,
+                                                    StepValPoints<FPTypeValue>& step_valpoints);
+  ValStats<FPTypeValue> get_error_stats(const FPTypeValue* phy_out,
+                                        StepValPoints<FPTypeValue>& step_valpoints);
 #endif
   void backup_surrogate_outs(const std::vector<FPTypeValue*> sur_out,
-                             std::vector<StepValPoints<FPTypeValue>>& step_valpoints) const;
+                             std::vector<StepValPoints<FPTypeValue>>& step_valpoints);
 
   const bool* predicate() const { return m_pred_loc_new; }
+
+  std::vector<ValStats<FPTypeValue>> get_error_stats(
+      const std::vector<FPTypeValue*> phy_out,
+      std::vector<StepValPoints<FPTypeValue>>& step_valpoints);
 
 protected:
   int gather_predicate(const bool* pred_loc, size_t num_pred_loc);
@@ -137,6 +150,13 @@ protected:
   bool* distribute_predicate(const AMSResourceType appDataLoc);
   /// Clear intermediate data for root rank to handle predicate update
   void clear_intermediate_info();
+  std::array<FPTypeValue, 2> sum_sqdev(const FPTypeValue* phy_out,
+                                       const FPTypeValue* sur_out,
+                                       const StepValPoints<FPTypeValue>& step_valpoints,
+                                       const std::array<FPTypeValue, 2>& avg);
+  std::vector<std::array<FPTypeValue, 2>> backup_validation_outs(
+    const std::vector<FPTypeValue*> phy_out,
+    std::vector<StepValPoints<FPTypeValue>>& step_valpoints);
 
 protected:
   MPI_Comm m_comm;
@@ -146,6 +166,7 @@ protected:
   const bool* m_pred_loc;
   bool* m_pred_loc_new;
   AMSResourceType m_appDataLoc;
+  std::vector<FPTypeValue*> m_sur; ///< surrogate output backup
 
   // For root rank
   std::vector<uint8_t> m_predicate_all;
@@ -181,11 +202,18 @@ VPCollector<FPTypeValue>::~VPCollector()
   if (m_pred_loc_new) {
 #if __STANDALONE_TEST__
     ams::ResourceManager::deallocate(m_pred_loc_new, m_appDataLoc);
+    for (size_t i = 0u; i < m_sur.size(); ++i) {
+      ams::ResourceManager::deallocate(m_sur[i], m_appDataLoc);
+    }
 #else
     auto &rm_d = ams::ResourceManager::getInstance();
     rm_d.deallocate(m_pred_loc_new, m_appDataLoc);
+    for (size_t i = 0u; i < m_sur.size(); ++i) {
+      rm_d.deallocate(m_sur[i], m_appDataLoc);
+    }
 #endif // __STANDALONE_TEST__
     m_pred_loc_new = nullptr;
+    m_sur.clear();
   }
   m_pred_loc = nullptr;
   m_num_pred_loc = 0ul;
@@ -378,15 +406,25 @@ bool VPCollector<FPTypeValue>::set_validation(
 }
 
 template <typename FPTypeValue>
-FPTypeValue sum_sqdev(
+std::array<FPTypeValue, 2> VPCollector<FPTypeValue>::sum_sqdev(
+  const FPTypeValue* phy_out,
+  const FPTypeValue* sur_out,
   const StepValPoints<FPTypeValue>& step_valpoints,
-  const FPTypeValue avg)
+  const std::array<FPTypeValue, 2>& avg)
 {
-  FPTypeValue sum = static_cast<FPTypeValue>(0);
+  std::array<FPTypeValue, 2> sum{static_cast<FPTypeValue>(0), static_cast<FPTypeValue>(0)};
 
+  size_t k = 0ul;
   for (auto& pt: step_valpoints.val_pts()) {
-    auto diff = std::abs(pt.m_o_phy - pt.m_o_sur) - avg;
-    sum += diff*diff;
+    for (; k < pt.m_pos; ++k) {
+      sum[0] += std::abs(phy_out[k] - sur_out[k]);
+    }
+    k++;
+    auto diff = std::abs(pt.m_o_phy - pt.m_o_sur) - avg[1];
+    sum[1] += diff*diff;
+  }
+  for (; k < m_num_pred_loc; ++k) {
+    sum[0] += std::abs(phy_out[k] - sur_out[k]);
   }
 
   return sum;
@@ -396,8 +434,14 @@ FPTypeValue sum_sqdev(
 template <typename FPTypeValue>
 void VPCollector<FPTypeValue>::backup_surrogate_outs(
   const FPTypeValue* sur_out,
-  StepValPoints<FPTypeValue>& step_valpoints) const
+  StepValPoints<FPTypeValue>& step_valpoints)
 {
+  m_sur.resize(1u, nullptr);
+#if __STANDALONE_TEST__
+  m_sur[0] = ams::ResourceManager::allocate<FPTypeValue>(m_num_pred_loc, m_appDataLoc);
+#endif // __STANDALONE_TEST__
+  std::memcpy(m_sur[0], sur_out, m_num_pred_loc);
+
   for (size_t i = 0ul; i < m_num_pred_loc; ++i) {
     if (m_pred_loc_new[i] != m_pred_loc[i]) {
       step_valpoints.add_pt(ValPoint<FPTypeValue>(i, sur_out[i]));
@@ -408,79 +452,88 @@ void VPCollector<FPTypeValue>::backup_surrogate_outs(
 }
 
 template <typename FPTypeValue>
-FPTypeValue backup_validation_outs(
+std::array<FPTypeValue, 2> VPCollector<FPTypeValue>::backup_validation_outs(
   const FPTypeValue* phy_out,
   StepValPoints<FPTypeValue>& step_valpoints)
 {
-  FPTypeValue sum = static_cast<FPTypeValue>(0);
+  std::array<FPTypeValue, 2> sum {static_cast<FPTypeValue>(0), static_cast<FPTypeValue>(0)};
 
+  size_t k = 0ul;
   for (auto& pt: step_valpoints.val_pts()) {
+    for (; k < pt.m_pos; ++k) {
+      sum[0] += std::abs(phy_out[k] - m_sur[0][k]);
+    }
+    k++;
     pt.m_o_phy = phy_out[pt.m_pos];
-    sum += std::abs(pt.m_o_phy - pt.m_o_sur);
+    sum[1] += std::abs(pt.m_o_phy - pt.m_o_sur);
   }
+
+  for (; k < m_num_pred_loc; ++k) {
+    sum[0] += std::abs(phy_out[k] - m_sur[0][k]);
+  }
+
   return sum;
 }
 
-ValStats<float> get_error_stats(
-  const float* phy_out,
-  StepValPoints<float>& step_valpoints,
-  MPI_Comm comm = MPI_COMM_WORLD)
+template <typename FPTypeValue>
+ValStats<FPTypeValue> VPCollector<FPTypeValue>::get_error_stats(
+  const FPTypeValue* phy_out,
+  StepValPoints<FPTypeValue>& step_valpoints)
 {
   const auto err_sum_loc = backup_validation_outs(phy_out, step_valpoints);
-  float err_sum_glo = static_cast<float>(0);
-  float err_avg_glo =  static_cast<float>(0);
+  std::array<FPTypeValue, 2> err_sum_glo {static_cast<FPTypeValue>(0), static_cast<FPTypeValue>(0)};
+  std::array<FPTypeValue, 2> err_avg_glo {static_cast<FPTypeValue>(0), static_cast<FPTypeValue>(0)};
 
-  unsigned err_cnt_loc = step_valpoints.get_num_points();
-  unsigned err_cnt_glo = 0u;
+  std::array<unsigned, 2> err_cnt_loc {static_cast<unsigned>(m_num_pred_loc - step_valpoints.get_num_points()),
+                                       static_cast<unsigned>(step_valpoints.get_num_points())};
+  std::array<unsigned, 2> err_cnt_glo {0u, 0u};
 
-  MPI_Allreduce(&err_cnt_loc, &err_cnt_glo, 1, MPI_UNSIGNED, MPI_SUM, comm);
-  MPI_Allreduce(&err_sum_loc, &err_sum_glo, 1, MPI_FLOAT, MPI_SUM, comm);
-  err_avg_glo = err_sum_glo / static_cast<float>(err_cnt_glo);
+  MPI_Allreduce(&err_cnt_loc[0], &err_cnt_glo[0], 2, MPI_UNSIGNED, MPI_SUM, m_comm);
+  MPI_Datatype mpi_dtype = MPI_FLOAT;
+  if (std::is_same<FPTypeValue, double>::value) {
+    mpi_dtype = MPI_DOUBLE;
+  } else //if (std::is_same<FPTypeValue, FPTypeValue>::value)
+  {
+    mpi_dtype = MPI_FLOAT;
+  }
 
-  float err_var_loc = sum_sqdev(step_valpoints, err_avg_glo)
-                      / static_cast<float>(err_cnt_glo);
-  float err_var_glo = 0;
-  MPI_Allreduce(&err_var_loc, &err_var_glo, 1, MPI_FLOAT, MPI_SUM, comm);
-  err_var_glo = err_var_glo/static_cast<float>(err_cnt_glo);
+  MPI_Allreduce(&err_sum_loc[0], &err_sum_glo[0], 2, mpi_dtype, MPI_SUM, m_comm);
+  err_avg_glo[0] = err_sum_glo[0] / static_cast<FPTypeValue>(err_cnt_glo[0]);
+  err_avg_glo[1] = err_sum_glo[1] / static_cast<FPTypeValue>(err_cnt_glo[1]);
 
-  return ValStats<float>(err_cnt_glo, err_avg_glo, err_var_glo);
+  const auto err_var_loc = sum_sqdev(phy_out, m_sur[0], step_valpoints, err_avg_glo);
+  //err_var_loc[0] /= static_cast<FPTypeValue>(err_cnt_glo[0]);
+  //err_var_loc[1] /= static_cast<FPTypeValue>(err_cnt_glo[1]);
+
+  std::array<FPTypeValue, 2> err_var_glo{0,0};
+  MPI_Allreduce(&err_var_loc[0], &err_var_glo[0], 2, mpi_dtype, MPI_SUM, m_comm);
+  err_var_glo[0] /= static_cast<FPTypeValue>(err_cnt_glo[0]);
+  err_var_glo[1] /= static_cast<FPTypeValue>(err_cnt_glo[1]);
+
+  return ValStats<FPTypeValue>(err_cnt_glo, err_avg_glo, err_var_glo);
 }
-
-ValStats<double> get_error_stats(
-  const double* phy_out,
-  StepValPoints<double>& step_valpoints,
-  MPI_Comm comm = MPI_COMM_WORLD)
-{
-  const auto err_sum_loc = backup_validation_outs(phy_out, step_valpoints);
-  double err_sum_glo = static_cast<double>(0);
-  double err_avg_glo =  static_cast<double>(0);
-
-  unsigned err_cnt_loc = step_valpoints.get_num_points();
-  unsigned err_cnt_glo = 0u;
-
-  MPI_Allreduce(&err_cnt_loc, &err_cnt_glo, 1, MPI_UNSIGNED, MPI_SUM, comm);
-  MPI_Allreduce(&err_sum_loc, &err_sum_glo, 1, MPI_DOUBLE, MPI_SUM, comm);
-  err_avg_glo = err_sum_glo / static_cast<double>(err_cnt_glo);
-
-  double err_var_loc = sum_sqdev(step_valpoints, err_avg_glo)
-                       / static_cast<double>(err_cnt_glo);
-  double err_var_glo = 0;
-  MPI_Allreduce(&err_var_loc, &err_var_glo, 1, MPI_DOUBLE, MPI_SUM, comm);
-  err_var_glo = err_var_glo/static_cast<double>(err_cnt_glo);
-
-  return ValStats<double>(err_cnt_glo, err_avg_glo, err_var_glo);
-}
-
 #endif // __STANDALONE_TEST__
+
 
 template <typename FPTypeValue>
 void VPCollector<FPTypeValue>::backup_surrogate_outs(
   const std::vector<FPTypeValue*> sur_out,
-  std::vector<StepValPoints<FPTypeValue>>& step_valpoints) const
+  std::vector<StepValPoints<FPTypeValue>>& step_valpoints)
 {
   const size_t dim = sur_out.size();
   step_valpoints.clear();
   step_valpoints.resize(dim);
+
+  m_sur.resize(dim);
+  for (size_t j = 0ul; j < dim; ++j) {
+#if __STANDALONE_TEST__
+    m_sur[j] = ams::ResourceManager::allocate<FPTypeValue>(m_num_pred_loc, m_appDataLoc);
+#else
+    auto &rm_a = ams::ResourceManager::getInstance();
+    m_sur[j] = rm_a.allocate<FPTypeValue>(m_num_pred_loc, m_appDataLoc);
+#endif // __STANDALONE_TEST__
+    std::memcpy(m_sur[j], sur_out[j], m_num_pred_loc);
+  }
 
   for (size_t i = 0ul; i < m_num_pred_loc; ++i) {
     if (m_pred_loc_new[i] != m_pred_loc[i]) {
@@ -496,13 +549,15 @@ void VPCollector<FPTypeValue>::backup_surrogate_outs(
 }
 
 template <typename FPTypeValue>
-std::vector<FPTypeValue> backup_validation_outs(
+std::vector<std::array<FPTypeValue, 2>> VPCollector<FPTypeValue>::backup_validation_outs(
   const std::vector<FPTypeValue*> phy_out,
   std::vector<StepValPoints<FPTypeValue>>& step_valpoints)
 {
   const size_t dim = step_valpoints.size();
 
-  std::vector<FPTypeValue> sum(dim, static_cast<FPTypeValue>(0));
+  std::vector<std::array<FPTypeValue, 2>> sum(
+    dim,
+    {static_cast<FPTypeValue>(0), static_cast<FPTypeValue>(0)});
 
   if (phy_out.size() != dim) {
     // exception
@@ -511,69 +566,61 @@ std::vector<FPTypeValue> backup_validation_outs(
   }
 
   for (size_t j = 0ul; j < dim; ++j) {
+    size_t k = 0ul;
     for (auto& pt: step_valpoints[j].val_pts()) {
+      for ( ; k < pt.m_pos; ++k) {
+        sum[j][0] += std::abs(phy_out[j][k] - m_sur[j][k]);
+      }
+      k++;
       pt.m_o_phy = phy_out[j][pt.m_pos];
-      sum[j] += std::abs(pt.m_o_phy - pt.m_o_sur);
+      sum[j][1] += std::abs(pt.m_o_phy - pt.m_o_sur);
+    }
+    for ( ; k < m_num_pred_loc; ++k) {
+      sum[j][0] += std::abs(phy_out[j][k] - m_sur[j][k]);
     }
   }
   return sum;
 }
 
-std::vector<ValStats<float>> get_error_stats(
-  const std::vector<float*> phy_out,
-  std::vector<StepValPoints<float>>& step_valpoints,
-  MPI_Comm comm = MPI_COMM_WORLD)
+
+template <typename FPTypeValue>
+std::vector<ValStats<FPTypeValue>> VPCollector<FPTypeValue>::get_error_stats(
+  const std::vector<FPTypeValue*> phy_out,
+  std::vector<StepValPoints<FPTypeValue>>& step_valpoints)
 {
   const auto err_sum_loc = backup_validation_outs(phy_out, step_valpoints);
   const size_t dim = err_sum_loc.size();
-  std::vector<float> err_sum_glo(dim, static_cast<float>(0));
-  std::vector<float> err_avg_glo(dim, static_cast<float>(0));
-  std::vector<float> err_var_glo(dim, static_cast<float>(0));
-  std::vector<ValStats<float>> stats(dim);
+  std::vector<std::array<FPTypeValue, 2>> err_sum_glo(dim, {static_cast<FPTypeValue>(0), static_cast<FPTypeValue>(0)});
+  std::vector<std::array<FPTypeValue, 2>> err_avg_glo(dim, {static_cast<FPTypeValue>(0), static_cast<FPTypeValue>(0)});
+  std::vector<std::array<FPTypeValue, 2>> err_var_glo(dim, {static_cast<FPTypeValue>(0), static_cast<FPTypeValue>(0)});
+  std::vector<ValStats<FPTypeValue>> stats(dim);
 
-  unsigned err_cnt_loc = step_valpoints.at(0).get_num_points();
-  unsigned err_cnt_glo = 0u;
+  std::array<unsigned, 2>  err_cnt_loc {m_num_pred_loc - step_valpoints.at(0).get_num_points(),
+                                        step_valpoints.at(0).get_num_points()};
+  std::array<unsigned, 2>  err_cnt_glo {0u, 0u};
 
-  MPI_Allreduce(&err_cnt_loc, &err_cnt_glo, 1, MPI_UNSIGNED, MPI_SUM, comm);
+  MPI_Allreduce(&err_cnt_loc[0], &err_cnt_glo[0], 2, MPI_UNSIGNED, MPI_SUM, m_comm);
 
-  for (size_t j = 0ul; j < dim; ++j) {
-    MPI_Allreduce(&err_sum_loc[j], &err_sum_glo[j], 1, MPI_FLOAT, MPI_SUM, comm);
-    err_avg_glo[j] = err_sum_glo[j] / static_cast<float>(err_cnt_glo);
-    float err_var_loc = sum_sqdev(step_valpoints[j], err_avg_glo[j])
-                         / static_cast<float>(err_cnt_glo);
-    MPI_Allreduce(&err_var_loc, &err_var_glo[j], 1, MPI_FLOAT, MPI_SUM, comm);
-    err_var_glo[j] = err_var_glo[j]/static_cast<float>(err_cnt_glo);
-    stats[j] = ValStats<float>(err_cnt_glo, err_avg_glo[j], err_var_glo[j]);
+  MPI_Datatype mpi_dtype = MPI_FLOAT;
+  if (std::is_same<FPTypeValue, double>::value) {
+    mpi_dtype = MPI_DOUBLE;
+  } else //if (std::is_same<FPTypeValue, float>::value)
+  {
+    mpi_dtype = MPI_FLOAT;
   }
 
-  return stats;
-}
-
-std::vector<ValStats<double>> get_error_stats(
-  const std::vector<double*> phy_out,
-  std::vector<StepValPoints<double>>& step_valpoints,
-  MPI_Comm comm = MPI_COMM_WORLD)
-{
-  const auto err_sum_loc = backup_validation_outs(phy_out, step_valpoints);
-  const size_t dim = err_sum_loc.size();
-  std::vector<double> err_sum_glo(dim, static_cast<double>(0));
-  std::vector<double> err_avg_glo(dim, static_cast<double>(0));
-  std::vector<double> err_var_glo(dim, static_cast<double>(0));
-  std::vector<ValStats<double>> stats(dim);
-
-  unsigned err_cnt_loc = step_valpoints.at(0).get_num_points();
-  unsigned err_cnt_glo = 0u;
-
-  MPI_Allreduce(&err_cnt_loc, &err_cnt_glo, 1, MPI_UNSIGNED, MPI_SUM, comm);
-
   for (size_t j = 0ul; j < dim; ++j) {
-    MPI_Allreduce(&err_sum_loc[j], &err_sum_glo[j], 1, MPI_DOUBLE, MPI_SUM, comm);
-    err_avg_glo[j] = err_sum_glo[j] / static_cast<double>(err_cnt_glo);
-    double err_var_loc = sum_sqdev(step_valpoints[j], err_avg_glo[j])
-                         / static_cast<double>(err_cnt_glo);
-    MPI_Allreduce(&err_var_loc, &err_var_glo[j], 1, MPI_DOUBLE, MPI_SUM, comm);
-    err_var_glo[j] = err_var_glo[j]/static_cast<double>(err_cnt_glo);
-    stats[j] = ValStats<double>(err_cnt_glo, err_avg_glo[j], err_var_glo[j]);
+    MPI_Allreduce(&err_sum_loc[j][0], &err_sum_glo[j][0], 2, mpi_dtype, MPI_SUM, m_comm);
+    err_avg_glo[j][0] = err_sum_glo[j][0] / static_cast<FPTypeValue>(err_cnt_glo[0]);
+    err_avg_glo[j][1] = err_sum_glo[j][1] / static_cast<FPTypeValue>(err_cnt_glo[1]);
+    std::array<FPTypeValue, 2> err_var_loc =
+      sum_sqdev(phy_out[j], step_valpoints[j], err_avg_glo[j], j);
+    //err_var_loc[0] /= static_cast<FPTypeValue>(err_cnt_glo[0]);
+    //err_var_loc[1] /= static_cast<FPTypeValue>(err_cnt_glo[1]);
+    MPI_Allreduce(&err_var_loc, &err_var_glo[j], 1, mpi_dtype, MPI_SUM, m_comm);
+    err_var_glo[j][0] /= static_cast<FPTypeValue>(err_cnt_glo[0]);
+    err_var_glo[j][1] /= static_cast<FPTypeValue>(err_cnt_glo[1]);
+    stats[j] = ValStats<FPTypeValue>(err_cnt_glo, err_avg_glo[j], err_var_glo[j]);
   }
 
   return stats;
